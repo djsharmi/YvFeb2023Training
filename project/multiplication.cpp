1444d937a42a46eca103caae1009704576b49f8b
nandhini161101 <nandhini.p@yantravision.com>
2023-02-20 13:09:10 +0530

    Revert "delete multiplication.cpp"
    This reverts commit 21b33cb0add30aadf7ef209484828aac308b2c44.

---
+++ project/multiplication.cpp
@@ -0,0 +1,13 @@
+int multiplication(int first_operand,int second_operand){
+
+   int multiplication_result;
+
+   for(int i=0;i<=second_operand;i++){
+
+      multiplication_result+=first_operand;
+
+   }
+
+   return multiplication_result;
+
+}
