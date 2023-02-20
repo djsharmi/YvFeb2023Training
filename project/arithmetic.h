3bd05202c7b6f140e83e85ccae1b33a81435fb39
nandhini161101 <nandhini.p@yantravision.com>
2023-02-20 11:57:20 +0530

    arithmetic file

---
+++ project/arithmetic.h
@@ -0,0 +1,7 @@
+int addition(int,int);		//addition function prototyping
+
+int multiplication(int,int);	//multiplication function prototyping
+
+int first_operand;		//declaration of first operand
+
+int second_operand;		//declaration of second operand
