94fc9e353fab663fac8bda11c6857c1aceb22c0c
Archana011201 <archana.g@yantravision.com>
2023-02-20 12:28:41 +0530

    adding main file

--- project/main.cpp
+++ project/main.cpp
@@ -6,9 +6,18 @@
 
 #include<iostream>		//header file for input and output streams
 
+#include "arithmetic.h"		//user defined header file for defining variables
 
 using namespace std;
 
+int addition(int g_first_operand,int g_second_operand){
+   int sum_result;		//declaring sum result
+
+   sum_result=g_first_operand+g_second_operand;	//adding two number store it in sum_result
+
+   return sum_result;		//returning sum result to main function
+
+}
 
 int multiplication(int g_first_operand,int g_second_operand){
 
