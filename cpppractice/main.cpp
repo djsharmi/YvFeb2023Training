fb8c32fc432438456c3871662a8d50428e00d147
nandhini161101 <nandhini.p@yantravision.com>
2023-03-06 12:09:43 +0530

    add cpppractice

---
+++ cpppractice/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int arr[] {12,23,34,34,45,56,67};
+    for (auto element : arr){
+        cout << element << " ";
+    }
+    return 0;
+}
